c5cba0f39a0825d6e8d209fcf5785957350cc759
MiguelNeyoy <miguelneyoycn@gmail.com>
2026-07-12 19:37:12 -0700

    Chore: se creo la estructura para comenzar con la infraestructura y crear las tablas en la base de datos

---
+++ SistemaGabinos.Infrastructure/DataBase/Context/Context.cs
@@ -0,0 +1,9 @@
+﻿//Aqui es donde se coloca el punto de entrada para la base de datos, es decir, donde se configura la conexión a la base de datos y se definen los DbSets
+//para las entidades del modelo de datos.
+
+namespace SistemaGabinos.Infrastructure.DataBase.Context
+{
+    public class Context
+    {
+    }
+}
